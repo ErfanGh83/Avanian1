117bb3ff833e9b5d101473190ab527b558dc2925
ErfanGh <erfan.ghasemian83@gmail.com>
2025-04-11 15:15:12 +0330

    feat: added functionality to the themSwitcher

--- src/components/Buttons/ThemeSwitcher/DarkButton.tsx
+++ src/components/Buttons/ThemeSwitcher/DarkButton.tsx
@@ -1,14 +1,26 @@
+"use client"
+
+import { useTheme } from 'next-themes'
 import React from 'react'
 import { BiMoon } from 'react-icons/bi'
 
 const DarkButton = () => {
-  return (
-    <button
-        className='size-8 flex items-center justify-center rounded-full bg-black shadow-xl hover:rotate-[45]'
-    >
-        <BiMoon size={24} color='white'/>
-    </button>
-  )
+
+    const { setTheme, resolvedTheme } = useTheme()
+
+    //currently in dark mode so no dark button needed
+    if (resolvedTheme === 'dark') {
+        return (<div></div>)
+    }
+
+    return (
+        <button
+            className='size-8 flex items-center justify-center rounded-full bg-black shadow-xl hover:rotate-[45]'
+            onClick={() => setTheme('dark')}
+        >
+            <BiMoon size={24} color='white' />
+        </button>
+    )
 }
 
 export default DarkButton;
--- src/components/Buttons/ThemeSwitcher/LightButton.tsx
+++ src/components/Buttons/ThemeSwitcher/LightButton.tsx
@@ -1,14 +1,26 @@
+"use client"
+
+import { useTheme } from 'next-themes'
 import React from 'react'
 import { BiSun } from 'react-icons/bi'
 
 const LightButton = () => {
-  return (
-    <button
-        className='size-8 flex items-center justify-center rounded-full shadow-xl bg-white'
-    >
-        <BiSun size={24} color='black'/>
-    </button>
-  )
+
+    const { setTheme, resolvedTheme } = useTheme()
+
+    //currently in light mode so no light button needed
+    if (resolvedTheme === 'light') {
+        return (<div></div>)
+    }
+
+    return (
+        <button
+            className='size-8 flex items-center justify-center rounded-full shadow-xl bg-white'
+            onClick={() => setTheme('light')}
+        >
+            <BiSun size={24} color='black' />
+        </button>
+    )
 }
 
 export default LightButton;
