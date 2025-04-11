dcedccc2dbfad078a85b2ed7fca9ec732e815a7d
ErfanGh <erfan.ghasemian83@gmail.com>
2025-04-11 15:27:43 +0330

    feat: added animation to them switch button

--- src/components/Buttons/ThemeSwitcher/DarkButton.tsx
+++ src/components/Buttons/ThemeSwitcher/DarkButton.tsx
@@ -1,11 +1,13 @@
 "use client"
 
+import { motion } from 'framer-motion'
 import { useTheme } from 'next-themes'
 import React from 'react'
 import { BiMoon } from 'react-icons/bi'
 
 const DarkButton = () => {
 
+    //a hook which manages themes
     const { setTheme, resolvedTheme } = useTheme()
 
     //currently in dark mode so no dark button needed
@@ -14,12 +16,19 @@ const DarkButton = () => {
     }
 
     return (
-        <button
-            className='size-8 flex items-center justify-center rounded-full bg-black shadow-xl hover:rotate-[45]'
+        <motion.button
+            className='size-8 flex items-center justify-center rounded-full bg-black shadow-xl hover:rotate-12 transition-all'
             onClick={() => setTheme('dark')}
+            initial={{opacity: 0, x: 45}}
+            animate={{opacity: 1, x: 0}}
+            transition={{ 
+                duration: 0.1,
+                delay: 0,
+                ease: "easeOut"
+            }}
         >
             <BiMoon size={24} color='white' />
-        </button>
+        </motion.button>
     )
 }
 
--- src/components/Buttons/ThemeSwitcher/LightButton.tsx
+++ src/components/Buttons/ThemeSwitcher/LightButton.tsx
@@ -1,11 +1,13 @@
 "use client"
 
+import { motion } from 'framer-motion'
 import { useTheme } from 'next-themes'
 import React from 'react'
 import { BiSun } from 'react-icons/bi'
 
 const LightButton = () => {
 
+    //a hook which manages themes
     const { setTheme, resolvedTheme } = useTheme()
 
     //currently in light mode so no light button needed
@@ -14,12 +16,19 @@ const LightButton = () => {
     }
 
     return (
-        <button
-            className='size-8 flex items-center justify-center rounded-full shadow-xl bg-white'
+        <motion.button
+            className='size-8 flex items-center justify-center rounded-full shadow-xl bg-white hover:rotate-12 transition-all'
             onClick={() => setTheme('light')}
+            initial={{opacity: 0, x: -45}}
+            animate={{opacity: 1, x: 0}}
+            transition={{ 
+                duration: 0.1,
+                delay: 0,
+                ease: "easeOut"
+            }}
         >
             <BiSun size={24} color='black' />
-        </button>
+        </motion.button>
     )
 }
 
