5aaeaabf810a07373bb7a7012de13c5414a07485
ErfanGh <erfan.ghasemian83@gmail.com>
2025-04-12 12:46:10 +0330

    feat: added meta data to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import { ThemeProviders } from "@/providers/ThemeProvider";
 import "./globals.css";
 
+export const metadata = {
+  title: "Avanian",
+  description: "Best app for awesome users",
+  keywords: ["Next.js", "Vazirmatn", "Tailwind"],
+  authors: [{ name: "Your Name", url: "https://your-site.com" }],
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html dir="rtl" lang="fa" suppressHydrationWarning>
