71a4786a4a063e78bfb1c930acd1dc7688340685
ErfanGh <erfan.ghasemian83@gmail.com>
2025-04-13 17:54:23 +0330

    fix: fixed name of the component in SignUpFormContainer.tsx and also improved ui of auth containers in different sizes

--- src/components/containers/auth-containers/LoginFormContainer.tsx
+++ src/components/containers/auth-containers/LoginFormContainer.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 const LoginFormContainer = () => {
   return (
     <div
-      className="size-4/5 flex flex-row bg-white text-black dark:bg-gray-800 dark:text-white border-2 border-gray-100 dark:border-gray-900 rounded-3xl m-auto shadow-2xl overflow-hidden"
+      className="size-full xl:size-4/5 flex flex-row bg-white text-black dark:bg-gray-800 dark:text-white border-2 border-gray-100 dark:border-gray-900 xl:rounded-3xl m-auto xl:shadow-2xl overflow-hidden"
     >
       <div
         className='w-1/2 h-full flex items-center justify-center'
--- src/components/containers/auth-containers/SignUpFormContainer.tsx
+++ src/components/containers/auth-containers/SignUpFormContainer.tsx
@@ -2,10 +2,10 @@ import { SignUpForm } from '@/components/forms/SignUpForm'
 import AuthSliders from '@/components/sliders/AuthSliders'
 import React from 'react'
 
-const LoginFormContainer = () => {
+const SignUpFormContainer = () => {
   return (
     <div
-      className="size-4/5 flex flex-row bg-white text-black dark:bg-gray-800 dark:text-white border-2 border-gray-100 dark:border-gray-900 rounded-3xl m-auto shadow-2xl overflow-hidden"
+      className="size-full xl:size-4/5 flex flex-row bg-white text-black dark:bg-gray-800 dark:text-white border-2 border-gray-100 dark:border-gray-900 xl:rounded-3xl m-auto xl:shadow-2xl overflow-hidden"
     >
       <div
         className='w-1/2 h-full flex items-center justify-center'
@@ -23,4 +23,4 @@ const LoginFormContainer = () => {
   )
 }
 
-export default LoginFormContainer
+export default SignUpFormContainer
