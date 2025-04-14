f162c2781dc76486154fe10fc1df5eee59fbf497
ErfanGh <erfan.ghasemian83@gmail.com>
2025-04-14 21:17:23 +0330

    fix: fixed typescript error in validateField

--- src/components/forms/form-components/ValidateField.ts
+++ src/components/forms/form-components/ValidateField.ts
@@ -2,9 +2,11 @@ import { FormField, formSchema } from "@/schema/SignUpSchema";
 import { FormData } from "@/schema/SignUpSchema";
 
 export const validateField = (
-    field: FormField,
-    value: FormData[FormField]
-  ): string | null => {
-    const result = formSchema.pick({ [field]: true }).safeParse({ [field]: value });
-    return result.success ? null : result.error.errors[0].message;
-  };
+  field: FormField,
+  value: FormData[FormField]
+): string | null => {
+  const fieldObj = { [field]: true } as { [key in FormField]?: true };
+
+  const result = formSchema.pick(fieldObj).safeParse({ [field]: value });
+  return result.success ? null : result.error.errors[0].message;
+};
