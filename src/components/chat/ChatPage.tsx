9338b957f3d1afe26467775ed00c1ccb54423f95
mohammadhosein-p <150898771+mohammadhosein-p@users.noreply.github.com>
2025-04-15 15:55:10 +0430

    feat: added messageInput

--- src/components/chat/ChatPage.tsx
+++ src/components/chat/ChatPage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import MessageContainer from "./MessageContainer";
+import MessageInput from "./MessageInput";
 
 export default function ChatPage() {
   const fetchData: string[] = [];
@@ -7,6 +8,7 @@ export default function ChatPage() {
   return (
     <div className="flex flex-col overflow-hidden h-screen bg-[#B3D433] p-4">
       <MessageContainer data={fetchData} />
+      <MessageInput />
     </div>
   );
 }
