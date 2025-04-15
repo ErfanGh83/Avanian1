3db782862d04642f9cf8bd1683cc2a3544a43670
mohammadhosein-p <150898771+mohammadhosein-p@users.noreply.github.com>
2025-04-15 22:21:09 +0430

    style: changed text for company section

--- src/components/landing/sections/CompanySection.tsx
+++ src/components/landing/sections/CompanySection.tsx
@@ -9,13 +9,13 @@ export default function CompanySection() {
 
   return (
     <>
-      <div className="bg-landing_bg1 dark:bg-dark1 w-full h-[60vh] absolute top-10 -z-20" />
+      <div className="bg-landing_bg1 dark:bg-dark1 w-full h-[40vh] absolute top-10 z-[-2]" />
 
       <motion.section
         ref={ref}
         className="relative bg-landing_bg1 dark:bg-dark1 flex flex-col-reverse md:flex-row items-center justify-around gap-6 mt-44 mb-0 pb-10 px-6 md:px-16"
       >
-        <div className="absolute bottom-[-10] z-[-3] bg-landing_bg1 dark:bg-dark1 w-full h-32 " />
+        <div className="absolute bottom-[-10] z-[-2] bg-landing_bg1 dark:bg-dark1 w-full h-[20vh] " />
         <motion.div
           variants={{
             hidden: { opacity: 0, x: 50 },
@@ -30,9 +30,10 @@ export default function CompanySection() {
             سرزمین آوانیان
           </h1>
           <p className="max-w-xl text-base md:text-lg leading-relaxed">
-            شرکت آوانیان با تکیه بر دانش فنی و تجربه‌ی تخصصی، در زمینه‌های
-            طراحی، توسعه و پیاده‌سازی نرم‌افزارهای سازمانی و هوشمند فعالیت
-            می‌کند و همواره کیفیت و نوآوری را سرلوحه‌ی خدمات خود قرار داده است.
+            سرزمین نخستین زیست بوم تخصصی اوقات و فراغت کودکان و نوجوانان ایران
+            زمین است. شخصیت های جهان داستان آوانیان روایت جدیدی از روش های
+            سازماندهی و برنامه ریزی خانواده ایرانی در زمان فراغتشان را بیان می
+            کنند.
           </p>
         </motion.div>
 
