1f6eb29cda47ed87ab1c32660725a2b0fa6a5fd6
mohammadhosein-p <150898771+mohammadhosein-p@users.noreply.github.com>
2025-04-15 23:06:39 +0430

    style: removed useInView and changed text and used SectionButton for ShopSection

--- src/components/landing/sections/ShopSection.tsx
+++ src/components/landing/sections/ShopSection.tsx
@@ -1,18 +1,20 @@
 "use client";
 
-import React, { useRef } from "react";
-import { motion, useInView } from "framer-motion";
+import React from "react";
+import { motion } from "framer-motion";
+import SectionButton from "./SectionButton";
 
 export default function ShopSection() {
-  const ref = useRef(null);
-  const isInView = useInView(ref, { margin: "-100px", amount: 0.3 });
+  // const ref = useRef(null);
+  // const isInView = useInView(ref, { margin: "-100px", amount: 0.3 });
 
   return (
     <motion.div
-      ref={ref}
-      className="relative bg-landing_bg4 items-center w-full mt-[15vh] md:mt-[35vh] px-4 pb-36"
+      // ref={ref}
+      id="shop"
+      className="relative bg-landing_bg3 items-center w-full mt-[15vh] md:mt-[30vh] px-4 pb-36"
     >
-      <div className="absolute top-[-15vh] md:top-[-35vh] right-0 w-full bg-landing_bg4 h-64 z-[-7]" />
+      <div className="absolute top-[-20vh] md:top-[-25vh] right-0 w-full bg-landing_bg3 h-[40vh] z-[-7]" />
 
       <div className="max-w-[1100px] gap-6 justify-around mx-auto flex flex-col items-center md:flex-row ">
         <motion.img
@@ -26,7 +28,8 @@ export default function ShopSection() {
             visible: { opacity: 1, x: 0 },
           }}
           initial="hidden"
-          animate={isInView ? "visible" : "hidden"}
+          whileInView="visible"
+          viewport={{ once: true }}
           transition={{ duration: 1, ease: "easeOut" }}
           className="w-[250px] md:w-[350px] h-auto"
         />
@@ -37,26 +40,19 @@ export default function ShopSection() {
             visible: { opacity: 1, x: 0 },
           }}
           initial="hidden"
-          animate={isInView ? "visible" : "hidden"}
+          whileInView="visible"
+          viewport={{ once: true }}
           transition={{ duration: 1, ease: "easeOut" }}
           className="flex flex-col items-start text-right"
         >
           <h3 className="text-4xl font-bold mb-2">رفتن به فروشگاه آوانیان</h3>
           <p className="max-w-[80vh]">
-            با مراجعه به فروشگاه آوانیان، میتوانید از جدیدترین محصولات و خدمات
-            ما دیدن کنید. چه به دنبال خرید آنلاین باشید و چه بخواهید از
-            پیشنهادهای ویژه ما استفاده کنید، همهچیز در فروشگاه آوانیان در دسترس
-            شماست. همین حالا به فروشگاه سر بزنید و از تجربه خریدی آسان و مطمئن
-            لذت ببرید!
+            «فروشگاه آوانیان»، به عنوان نخستین فروشگاه اینترنتی و Market Place
+            تخصصی حوزه اوقات فراغت کودک و نوجوان فعالیت خود را از بهار ۱۴۰۱ به
+            نام «شبکه اوقات فراغت» آغاز نموده است.
           </p>
-          <motion.button
-            className="px-8 py-3 bg-turquoise dark:bg-dark1 text-white my-6 rounded-3xl"
-            whileHover={{ scale: 1.2, y: -10, opacity: 0.95 }}
-            whileTap={{ scale: 0.9, y: -5 }}
-            transition={{ duration: 0.3 }}
-          >
-            رفتن به بات
-          </motion.button>
+
+          <SectionButton href="/shop">رفتن به فروشگاه</SectionButton>
         </motion.div>
       </div>
     </motion.div>
