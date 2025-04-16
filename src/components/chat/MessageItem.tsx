14ea56cf944c7067b3b78d023935bcaa5225a15c
ErfanGh <erfan.ghasemian83@gmail.com>
2025-04-16 20:17:38 +0330

    fix: removed pfps

--- src/components/chat/MessageItem.tsx
+++ src/components/chat/MessageItem.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import OwlPfp from "./owl-pfp";
 
 type Props = {
   children: string;
@@ -16,36 +15,12 @@ export default function MessageItem({ sender, children }: Props) {
         className={`w-full h-full items-end max-w-2xl flex gap-2 overflow-hidden 
           ${sender === "user" ? "flex-row" : "flex-row-reverse"}`}
       >
-        <div className="h-full flex flex-col items-end justify-end">
-          {sender === "bot" ? (
-            <OwlPfp />
-          ) : (
-            <div className="size-16 rounded-full flex items-center justify-center bg-blue-300"></div>
-          )}
-        </div>
 
         <div
           className={`max-w-[75%] h-fit min-h-[75%] bg-t/30 relative rounded-t-xl ${
             sender == "user" ? "bg-dark2 rounded-bl-xl" : "bg-avanianBlue rounded-br-xl"
           }`}
         >
-          <div
-            className={`absolute ${
-              sender == "bot" ? "left-[-4px] top-[45px]" : "right-[-4px]"
-            } bottom-0`}
-          >
-            <svg
-              width="10"
-              height="10"
-              viewBox="0 0 30 40"
-              xmlns="http://www.w3.org/2000/svg"
-              className={`${
-                sender == "user" ? "fill-dark2 rotate-180" : "fill-avanianBlue"
-              }`}
-            >
-              <polygon points="0,5 10,0 10,10" />
-            </svg>
-          </div>
 
           <div className="w-full p-4 break-words overflow-wrap-anywhere">
             <h3>{children}</h3>
