e37bf61151fb0894a0ebe42a8d22f32a31b2f85e
ErfanGh <erfan.ghasemian83@gmail.com>
2025-05-14 18:24:38 +0330

    feat: fixed text size in landing

--- src/components/landing/sections/CompanySection.tsx
+++ src/components/landing/sections/CompanySection.tsx
@@ -28,7 +28,7 @@ export default function CompanySection() {
             transition={{ duration: 1.2, ease: "easeOut" }}
             className="flex flex-col items-start text-right"
           >
-            <h1 className="text-4xl md:text-5xl max-w-[400px] md:max-w-[700px] font-bold pb-4 text-outline-lg">
+            <h1 className="text-3xl md:text-4xl max-w-[400px] md:max-w-[500px] font-bold pb-4 text-outline-lg">
               سامانه مدیریت و برنامه ریزی اوقات فراغت کودک و نوجوان
             </h1>
             <p className="text-lg text-justify max-w-[300px] md:max-w-[465px] text-outline-md">
