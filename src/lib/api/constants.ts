abe56700ab2ced9f31355a0decab45afc83109e7
ErfanGh <erfan.ghasemian83@gmail.com>
2025-05-15 13:05:12 +0330

    fixed endpoints

--- src/lib/api/constants.ts
+++ src/lib/api/constants.ts
@@ -1,8 +1,8 @@
 export const BASE_URL = "https://avanian-back.liara.run";
 export const API_ENDPOINTS = {
   USERS: "/users/",
-  REQUEST_OTP: "/request-otp/",
-  VERIFY_OTP: "/verify-otp/",
+  REQUEST_OTP: "/request-otp",
+  VERIFY_OTP: "/verify-otp",
   SESSIONS: "/chat/sessions/",
   SEND_MESSAGE: "/chat/sessions/"
 };
